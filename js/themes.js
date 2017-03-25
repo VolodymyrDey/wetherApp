99ff1646f3511865d489ce9ffa9fc316057fea6e
Volodia Dei <Volodia Dei>
2017-03-25 12:43:34 +0200

    Romoved changes from tvv

--- js/themes.js
+++ js/themes.js
@@ -2,7 +2,7 @@
 
 function set_color_bg() {
   var color = document.getElementById("color_bg").value;
-  setBgstyle("body{background-color:" + color + "} #search_field{background-color:" + color + "} .search_button{background-color:" + color + "}");
+  set_style("body{background-color:" + color + "} #search_field{background-color:" + color + "} .search_button{background-color:" + color + "}");
 }
 
 function set_picture_bg() {
@@ -15,45 +15,16 @@ function set_picture_bg() {
 
 function set_bg_img(e) {
   if (e.keyCode === 13) {
-      setBgstyle("body{background-image: url(" + document.getElementById("link_bg").value + "); background-color: white;}");
+      set_style("body{background-image: url(" + document.getElementById("link_bg").value + "); background-color: white;}");
       var link_input = document.getElementById("link_bg");
       if(link_input) tbl.parentNode.removeChild(link_input);
       return false;
   }
 }
 
-function setBgstyle(style) {
+function set_style(style) {
   var x = document.createElement("STYLE");
   var t = document.createTextNode(style);
   x.appendChild(t);
   document.head.appendChild(x);
-  if(style) {
-    saveChoice(style);
-  }
-}
-
-//save style with bgImage or bgColor to LocalStorage
-function saveChoice(value) {
-  if (storageAvailable('localStorage')) {
-    localStorage.setItem('bg', value);
-  }
-}
-document.addEventListener('DOMContentLoaded', function() {
-  if(localStorage.getItem !== 'undefined') {
-    set_style(localStorage.getItem('bg'));
-  }
-});
-
-//check if Web Storage is available
-function storageAvailable(type) {
-	try {
-		const storage = window[type],
-			x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch(e) {
-		return false;
-	}
 }
